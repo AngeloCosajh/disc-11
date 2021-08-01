23ed3114b1b83abaf5bb6a3c7db1b42a36035607
AngeloCosajh <60151968+AngeloCosajh@users.noreply.github.com>
2021-08-01 11:40:32 +0200

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -7,14 +7,14 @@ import { IMessage } from "../../typings";
 @DefineCommand({
     description: "Send the bot's invite link",
     disable: disableInviteCmd,
-    name: "invite",
-    usage: "{prefix}invite"
+    name: "invita",
+    usage: "{prefix}invita"
 })
 export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
         message.channel.send(
             createEmbed("info")
-                .addField(`${this.client.user!.tag} - Invite Link`, `**[Click here to invite this bot](${await this.client.generateInvite({ permissions: 53857345 })})**`)
+                .addField(`${this.client.user!.tag} - Link di invito`, `**[Clicca qui per invitare questo bot](${await this.client.generateInvite({ permissions: 53857345 })})**`)
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
     }
 }
