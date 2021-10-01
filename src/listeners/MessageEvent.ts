94a12bd9efab39c069a4be8f247e9f1c01b29313
AngeloCosajh <60151968+AngeloCosajh@users.noreply.github.com>
2021-10-01 19:29:06 +0200

    Update MessageEvent.ts

--- src/listeners/MessageEvent.ts
+++ src/listeners/MessageEvent.ts
@@ -13,7 +13,7 @@ export class MessageEvent extends BaseListener {
 
         if ((await this.getUserFromMention(message.content))?.id === message.client.user?.id) {
             return message.channel.send(
-                createEmbed("info", `👋 Hi, my name is **${this.client.user!.username}** a simple Discord music bot, use **\`${this.client.config.prefix}help\`** to have a look at my commands`)
+                createEmbed("info", `👋 Ciao, il mio nome è **${this.client.user!.username}** un semplice bot musicale Discord, usa **\`${this.client.config.prefix}help\`** per dare un'occhiata ai miei comandi`)
             );
         }
     }
